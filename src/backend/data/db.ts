3d1ce2c3e20b51433c0116e72f382f5f1788c18d
abbydise <abbydisedev@gmail.com>
2026-06-07 19:20:15 -0400

    Added DB connection

---
+++ src/backend/data/db.ts
@@ -0,0 +1,10 @@
+import postgres from 'postgres';
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('Missing database url');
+}
+
+const connectionString : string = process.env.DATABASE_URL;
+const sql = postgres(connectionString);
+
+export default sql;
