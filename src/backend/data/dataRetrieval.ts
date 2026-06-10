e503de8636fb74c98a0eb0de60ed3ce14ae5f23b
abbydise <abbydisedev@gmail.com>
2026-06-10 16:41:44 -0400

    made progress on formatting the tables

--- src/backend/data/dataRetrieval.ts
+++ src/backend/data/dataRetrieval.ts
@@ -112,36 +112,61 @@ const getAllArticles = async () => {
 const parseContentHTML = (article : Record<string, string>) => {
     const $ = cheerio.load(article.content);
 
+    $("#navbox").remove()
+    $("#toc").remove();
+
     const sections = $('h2');
     // console.log(sections);
 
     sections.each((i, section) => {
         const textArray : string[] = []
         let tableArray : string[] = [];
-        const content = $(section).nextUntil('h2');
-        content.each((i, element) => {
-            if (!("tagName" in element)) return;
-            const tag : string = element.tagName.toLowerCase();
-            if (tag == "p") {
-                textArray.push($(element).text().trim());
-            } else if (tag == "table" && $(element).attr("id") == "infoboxtable") {
-            } else {
-                tableArray = parseTableData($, element);
+        const sectionTitle : string = $(section).text().trim();
+        if (!sectionTitle.includes("Calendar") && !sectionTitle.includes("Stage") && !sectionTitle.includes("Quotes")) {
+            // console.log($(section).text().trim());
+            const content = $(section).nextUntil('h2');
+            content.each((i, element) => {
+                if (!("tagName" in element)) return;
+                const tag: string = element.tagName.toLowerCase();
+                if (tag == "p") {
+                    textArray.push($(element).text().trim());
+                } else if (tag == "ul") {
+                    parseListData($, element);
+                } else if ($(element).attr("id") == "infoboxtable") {
+
+                } else {
+                    if ($(element).attr("class") != "squote") {
+                        tableArray = parseTableData($, element);
+                    }
+                }
+            })
+            // console.log(textArray.join(" "));
+
+            if (tableArray.length != 0) {
+                console.log(tableArray.join("\n"));
             }
-        })
-        // console.log(textArray.join(" "));
-        console.log(tableArray.join("\n"));
+        }
     })
+}
+
+const parseListData = ($ : CheerioAPI, element : Element) => {
+
 }
 
 const parseTableData = ($ : CheerioAPI, element : Element) : string[] => {
     const tableArray : string[] = [];
 
-    const headers = $(element).find("th");
+    const headers = $(element).find("tbody > tr > th");
     const headersArray : string[] = [];
     headers.each((i, header) => {
-        headersArray.push($(header).text().trim());
+        if ($(header).attr("class") != "unsortable" && $(header).text().trim().toLowerCase() != "image") {
+            headersArray.push($(header).text().trim());
+        }
     })
+
+    if (headersArray.length == 0) {
+        return [];
+    }
     const headersString = "| " + headersArray.join(" | ") + " |";
     tableArray.push(headersString);
 
@@ -149,28 +174,62 @@ const parseTableData = ($ : CheerioAPI, element : Element) : string[] => {
     const separatorString = "| " + separatorArray.join(" | ") + " |";
     tableArray.push(separatorString);
 
-    const rows = $(element).find("tr");
+    const rows = $(element).find("tbody > tr");
     rows.each((i, row) => {
         const tableDataArray: string[] = [];
-        const data = $(row).find("td");
+        if ($(row).children("td").length == 0) {
+            return;
+        }
+
+        const rowHeaders = $(row).children("th");
+        if (rowHeaders.length != 0) {
+            rowHeaders.each((i, header) => {
+                tableDataArray.push($(header).text().trim());
+            })
+        }
+
+        const data = $(row).children("td");
         data.each((i, value) => {
-            if ($(value).text() !== " " && $(value).text().length > 0 && $(value).text().length < 250) tableDataArray.push($(value).text().trim());
+            const breaks = $(value).find("br");
+            if (breaks.length != 0) {
+                let htmlString = $(value).html();
+                if (htmlString) {
+                    htmlString = htmlString.replaceAll("<br>", "/")
+                    const $cleanedString = cheerio.load(htmlString);
+
+                    const text = $cleanedString().text().trim();
+                    if (text.length > 0) {
+                        tableDataArray.push(text);
+                    }
+                }
+            } else {
+                $(value).find("img").remove();
+                $(value).find("style").remove();
+                $(value).find("[style*='display: none']").remove();
+                const textContent = $(value).text().trim();
+                if (textContent.length > 0) {
+                    tableDataArray.push(textContent);
+                }
+            }
         })
-        const rowString = "| " + tableDataArray.join(" | ") + " |";
-        tableArray.push(rowString);
+
+        if (tableDataArray.length > 0) {
+            const rowString = "| " + tableDataArray.join(" | ") + " |";
+            tableArray.push(rowString);
+        }
     })
 
     return tableArray;
 }
 
-const parseInfoboxTableData = ($ : CheerioAPI, element : Element) => {
-
-}
-
 const testHTMLParser = async () => {
     await getAllArticles();
 
-    parseContentHTML(articleArray[0]);
+    // for (let article of articleArray) {
+    //     // console.log(article.title);
+    //     parseContentHTML(article);
+    // }
+    parseContentHTML(articleArray[6]);
 }
 
 testHTMLParser();
