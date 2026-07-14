cfe0e641d7ca063ed0c9a32bf14e3752fc65a127
Abby Dise <abbydisedev@gmail.com>
2026-07-14 14:38:26 -0400

    added the submit functionality to actually send the questions to the AI

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,7 +1,47 @@
+import Textbox from '../components/textbox';
+import Chatbox from '../components/chatbox';
+
+import { useState } from 'react';
+
+type Message = {
+    origin: "user" | "system",
+    content: string
+}
+
 export default function Home() {
+    const [messageHistory, setMessageHistory ] = useState<Array<Message>>([]);
+
+    const handleSubmit = async (question : string) => {
+        const body : Message = {origin: "user", content: question};
+
+        setMessageHistory((prev) => [...prev, body]);
+
+        const response = await fetch('/api/response', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": 'application/json'
+            }
+        })
+
+        if (response.ok) {
+            const content = await response.json();
+            const answer = content.answer;
+
+            setMessageHistory((prev) => [...prev, {origin: 'system', content: answer}]);
+        } else {
+            const content = await response.json();
+            const error = content.error;
+            console.error(error);
+
+            setMessageHistory((prev) => [...prev, {origin: 'system', content: 'An error has occurred. Please try again.'}]);
+        }
+    }
+
   return (
       <div>
         <h1>Home Page</h1>
+          <div><Textbox handleSubmit={handleSubmit}/></div>
       </div>
   )
 }
